775b2680721f8e761fef8af03cbc9686ee458d2f
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@gmial.com>
2019-09-19 11:47:51 +0200

    remove models module

--- src/modules/models/model.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-import { ModelEntity } from './model.entity';
-
-@EntityRepository(ModelEntity)
-export class ModelRepository extends Repository<ModelEntity> {}
