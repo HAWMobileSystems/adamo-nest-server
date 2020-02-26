81b9b6d447b449b87052d18b76bed9198b3c8046
Lukas <lukas.rissmann@web.de>
2020-02-26 12:51:19 +0100

    quickest code transition

--- src/modules/tutorial/tg_intro/tg_intro.repository.ts
+++ src/modules/tutorial/tg_intro/tg_intro.repository.ts
@@ -4,3 +4,21 @@ import { Tg_IntroEntity } from './tg_intro.entity';
 
 @EntityRepository(Tg_IntroEntity)
 export class Tg_IntroRepository extends Repository<Tg_IntroEntity> {}
+
+// var request = require('request');
+// var options = {
+//   'method': 'PUT',
+//   'url': 'http://localhost:3330/tg_intro/',
+//   'headers': {
+//     'Content-Type': 'application/x-www-form-urlencoded'
+//   },
+//   form: {
+//     'userid': '2c49c756-a64b-4032-946b-31e407f51875',
+//     'catName': 'Professional',
+//     'tg_intro_is_finished': 'true'
+//   }
+// };
+// request(options, function (error, response) { 
+//   if (error) throw new Error(error);
+//   console.log(response.body);
+// });
