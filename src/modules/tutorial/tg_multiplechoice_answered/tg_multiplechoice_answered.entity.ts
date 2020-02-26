47c3a5d07ab518ca2a643a5929a9cddd51e07425
Lukas <lukas.rissmann@web.de>
2020-02-26 12:54:47 +0100

    Refractoring for tg_multiplechoice_answered.*

--- src/modules/tutorial/tg_multiplechoice_answered/tg_multiplechoice_answered.entity.ts
+++ src/modules/tutorial/tg_multiplechoice_answered/tg_multiplechoice_answered.entity.ts
@@ -1,10 +1,8 @@
-import { Entity, Column,PrimaryGeneratedColumn, ManyToOne, OneToMany } from "typeorm";
+import { Entity, Column,PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { AbstractEntity } from '../../../common/abstract.entity';
-import { type } from "os";
 import { ApiModelProperty } from "@nestjs/swagger";
 import { Tg_Multiplechoice_AnsweredDto } from "./dto/tg_multiplechoice_answeredDto";
 import { Multiplechoice_Question_AnswerEntity } from "../multiplechoice_question_answer/multiplechoice_question_answer.entity";
-import { Tg_MultiplechoiceEntity } from "../tg_multiplechoice/tg_multiplechoice.entity";
 import { Exclude } from "class-transformer";
 
 @Entity({name:'tg_multiplechoice_answered'})
@@ -26,8 +24,6 @@ export class Tg_Multiplechoice_AnsweredEntity extends AbstractEntity<Tg_Multiple
     @ApiModelProperty()
     @Column()
     public tg_multiplechoice_answered_answerd: boolean;
-
-
     
     @Exclude()
     dtoClass = Tg_Multiplechoice_AnsweredDto;
