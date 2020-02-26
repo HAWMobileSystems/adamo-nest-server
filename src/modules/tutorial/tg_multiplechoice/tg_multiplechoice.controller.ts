8d29e8bea9ae49f82535e1d36a32ff02baf9c1f5
Lukas <lukas.rissmann@web.de>
2020-02-26 13:44:59 +0100

    Added tg_multiplechoice/solve/

--- src/modules/tutorial/tg_multiplechoice/tg_multiplechoice.controller.ts
+++ src/modules/tutorial/tg_multiplechoice/tg_multiplechoice.controller.ts
@@ -13,7 +13,6 @@ import {
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiUseTags } from '@nestjs/swagger';
 import { Tg_MultiplechoiceService } from './tg_multiplechoice.service';
-import { Tg_MultiplechoiceEntity } from './tg_multiplechoice.entity';
 
 @Controller('tg_multiplechoice')
 @ApiUseTags('tg_multiplechoice')
@@ -26,49 +25,12 @@ export class Tg_MultiplechoiceController {_
         return this.tg_multiplechoiceService.getMultiplechoiceQs(id,cat,lang)
     }
     
-    @Put(':user_id/solved/:qs_id/:answ')
-    solveMultipleChoice(@Param('user_id') user_id,@Param('qs_id') qs_id,@Param('answ') answers:string[]){
+    @Post('/solve/')
+    solveMultipleChoice(@Body() data:any){
+        console.log(data)
+        let user_id
+        let qs_id
+        let answers
         return this.tg_multiplechoiceService.solveMultiplechoice(user_id,qs_id,answers)
     }
-
-
-    /**
-     * 
-     
-    @Get()
-    listRoles() {
-        return this.tg_multiplechoiceService.find();
-    }
-
-     /**
-     * We use this also for password?
-     * 
-     * @param id 
-     * @param userData 
-     
-    @Put(':id/update')
-    async update(@Param('id') id, @Body() testData: Tg_MultiplechoiceEntity): Promise<any> {
-        // userData.id = Number(id);
-        // Set the roleData.id because it is missing in Data from Client?!
-        testData.id = id;
-        console.log('Update #' + testData.id)
-
-        return this.tg_multiplechoiceService.update(testData);
-    }  
-
-    /**
-     * 
-     * @param entity 
-     
-    @Post()
-    create(@Body() entity: Tg_MultiplechoiceEntity) {
-        this.tg_multiplechoiceService.create(entity);
-    }
-
-    @Delete(':id/delete')
-    async delete(@Param('id') id): Promise<any> {
-      return this.tg_multiplechoiceService.delete(id);
-      // Maybe deleting all role entries in Permissions with this role and set them back to Default ist an valid option? TODO
-    }  
-*/
 }
