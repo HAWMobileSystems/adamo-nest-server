beff29164bcdb808eab489f4ed8e4bdc295febe1
Lukas <lukas.rissmann@web.de>
2020-02-26 13:44:38 +0100

    Revert "Revert "Minor Refractoring""
    This reverts commit a98c930d0b5927030ea2d5079b494223fac58c5f.

--- src/modules/tutorial/tg_intro/tg_intro.controller.ts
+++ src/modules/tutorial/tg_intro/tg_intro.controller.ts
@@ -21,22 +21,12 @@ import { Tg_IntroEntity } from './tg_intro.entity';
 @ApiBearerAuth()
 export class Tg_IntroController {_
     constructor(private readonly tg_introService: Tg_IntroService) {}
-    
-    @Put(':user_id/:cat_name')
-    putUserTutorialFinished(@Param('user_id')user_id,@Param('cat_name') cat_name){
-        console.log("putUserTutorial")
-        return this.tg_introService.putUserTutorialFinished(user_id,cat_name,true)
-    }
-        
+       
     @Post('')
     putUserTutorialFinishedAlternative(@Body() data:any){
-        //,@Body('catName') catName:any,@Body('tg_intro_is_finished') tg_intro_is_finished:any){
-        //console.log("Tutorial-Alternative")
-        //console.log(data)
         let user_id  = data.userid
         let cat_name = data.catName
         let finished = true
-        //console.log(data)
         return this.tg_introService.putUserTutorialFinished(user_id,cat_name,finished)
     }
 }
