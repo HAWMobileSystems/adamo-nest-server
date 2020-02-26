dae204cc1563a9c5bb7bedddcc4ba36f7c599831
Lukas <lukas.rissmann@web.de>
2020-02-26 11:58:36 +0100

    even quicker fix

--- src/modules/tutorial/tg_intro/tg_intro.controller.ts
+++ src/modules/tutorial/tg_intro/tg_intro.controller.ts
@@ -24,16 +24,17 @@ export class Tg_IntroController {_
     
     @Put(':user_id/:cat_name')
     putUserTutorialFinished(@Param('user_id')user_id,@Param('cat_name') cat_name){
+        console.log("putUserTutorial")
         return this.tg_introService.putUserTutorialFinished(user_id,cat_name,true)
     }
         
-    // @Put('')
-    // putUserTutorialFinished(@Body('data') data:any){//,@Body('catName') catName:any,@Body('tg_intro_is_finished') tg_intro_is_finished:any){
-
-    //  //     let user_id  = userid
-    // //     let cat_name = catName
-    // //     let finished = tg_intro_is_finished
-    //     console.log(data)
-    //    // return this.tg_introService.putUserTutorialFinished(user_id,cat_name,finished)
-    // }
+    @Put('')
+    putUserTutorialFinishedAlternative(@Body('data') data:any){//,@Body('catName') catName:any,@Body('tg_intro_is_finished') tg_intro_is_finished:any){
+        console.log("Tutorial-Alternative")
+     //     let user_id  = userid
+    //     let cat_name = catName
+    //     let finished = tg_intro_is_finished
+        console.log(data)
+       // return this.tg_introService.putUserTutorialFinished(user_id,cat_name,finished)
+    }
 }
