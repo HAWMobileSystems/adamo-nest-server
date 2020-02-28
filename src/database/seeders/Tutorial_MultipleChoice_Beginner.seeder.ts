fcef155ff9c78bf05b634eeb1383e409dc442f7f
Lukas <lukas.rissmann@web.de>
2020-02-28 19:47:02 +0100

    Refractoring for Beginner Seeder

--- src/database/seeders/Tutorial_MultipleChoice_Beginner.seeder.ts
+++ src/database/seeders/Tutorial_MultipleChoice_Beginner.seeder.ts
@@ -1,25 +1,9 @@
 import { Seeder, Factory } from 'typeorm-seeding';
-import { IntroEntity } from '../../modules/tutorial/intro/intro.entity';
-import { Connection, getRepository, getConnection, AdvancedConsoleLogger } from 'typeorm';
-import { Modelling_QuestionEntity } from '../../modules/tutorial/modelling_question/modelling_question.entity';
+import { Connection, getRepository } from 'typeorm';
 import { CategoryEntity } from '../../modules/tutorial/category/category.entity';
-import { Modelling_RulesEntity} from '../../modules/tutorial/modelling_rules/modelling_rules.entity';
-import { Modelling_Question_RulesEntity } from '../../modules/tutorial/modelling_question_rules/modelling_question_rules.entity';
 import { Multiplechoice_QuestionEntity } from '../../modules/tutorial/multiplechoice_question/multiplechoice_question.entity';
 import { Multiplechoice_Question_AnswerEntity } from '../../modules/tutorial/multiplechoice_question_answer/multiplechoice_question_answer.entity';
-import { TestEntity } from '../../modules/tutorial/test/test.entity';
-import { UserEntity } from '../../modules/user/user.entity';
-import { Tg_IntroEntity } from '../../modules/tutorial/tg_intro/tg_intro.entity';
-import { Tg_ModellingEntity } from '../../modules/tutorial/tg_modelling/tg_modelling.entity';
-import { Tg_MultiplechoiceEntity } from '../../modules/tutorial/tg_multiplechoice/tg_multiplechoice.entity';
-import { Tg_Multiplechoice_AnsweredEntity } from '../../modules/tutorial/tg_multiplechoice_answered/tg_multiplechoice_answered.entity';
-import uuid = require('uuid');
-import { Multiplechoice_Question_AnswerDto } from 'modules/tutorial/multiplechoice_question_answer/dto/Multiplechoice_Question_AnswerDto';
-enum tg{
-    beginner,
-    advanced,
-    professional
-}
+
 export default class SeedMultipleChoiceQuestionBeginner implements Seeder {
     public async run(factory: Factory, connection: Connection): Promise<any> {
 
