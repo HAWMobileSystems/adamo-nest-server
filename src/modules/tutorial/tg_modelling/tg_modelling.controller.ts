947d7c1ba46f9924e343b6d8ad5cf842af8eed66
Lukas <lukas.rissmann@web.de>
2020-03-01 16:33:17 +0100

    Refractoring for tg_modelling.*

--- src/modules/tutorial/tg_modelling/tg_modelling.controller.ts
+++ src/modules/tutorial/tg_modelling/tg_modelling.controller.ts
@@ -2,19 +2,13 @@
 
 import {
     Get,
-    HttpCode,
-    HttpStatus,
     Controller,
     Post,
     Body,
-    Delete,
     Param,
-    Put,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiUseTags } from '@nestjs/swagger';
-import { Tg_ModellingEntity } from './tg_modelling.entity';
 import { Tg_ModellingService } from './tg_modelling.service';
-import { lawngreen } from 'color-name';
 
 @Controller('tg_modelling')
 @ApiUseTags('tg_modelling')
@@ -37,25 +31,8 @@ export class Tg_ModellingController {_
      */
     @Post('solve/:lang')
     solveSpecificModellingQuestion(@Param('lang') lang, @Body() data:any){
-        console.log("Solve - tg_modelling")
-        // console.log(data.user_choice)
-        let user_id 
-        let qs_id 
-        let xml
-        console.log(data) 
-        let answer:ReturnFromModQs = new ReturnFromModQs(data.xml,data.userid,data.qs_id)
-        // data.forEach(element => {
-        //     if(element.key == 'userid'){
-        //         user_id = element.value
-        //     }else if(element.key == 'qs_id'){
-        //         qs_id = element.value
-        //     }else {
-        //         xml = element.value
-        //     }
-        // });
-        lang = 'de'
-        let dur = 10000
-        return this.tg_modellingService.solveQuestion(answer.user_id,answer.qs_id,answer.xml,lang,dur)
+        let answer:ReturnFromModQs = new ReturnFromModQs(data.xml,data.userid,data.qs_id,data.duration)
+        return this.tg_modellingService.solveQuestion(answer.user_id,answer.qs_id,answer.xml,lang,answer.duration)
     }
 
     @Get('modellingQsWithRuleTest')
@@ -68,9 +45,11 @@ class ReturnFromModQs{
     xml: string
     user_id: string
     qs_id: string
-    constructor(xml_in,uid_in,qsid_in){
+    duration: number
+    constructor(xml_in,uid_in,qsid_in,dur){
         this.xml = xml_in;
         this.user_id = uid_in;
+        this.duration = dur;
         this.qs_id = qsid_in;
     }
 }
